108289f218e1bbe41ebd829abbc59e080b968d26
Fleeym <61891787+Fleeym@users.noreply.github.com>
2024-02-04 22:43:22 +0200

    update random_string

--- src/random_string.cpp
+++ src/random_string.cpp
@@ -1,22 +1,18 @@
 #include "random_string.hpp"
+#include <string_view>
 
 namespace nongd {
     std::string random_string(size_t length)
     {
-        auto randchar = []() -> char
-        {
-            const char charset[] =
-            "0123456789"
-            "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-            "abcdefghijklmnopqrstuvwxyz";
-            const size_t max_index = (sizeof(charset) - 1);
-            std::random_device rd;
-            std::mt19937_64 gen(rd());
-            std::uniform_int_distribution<> dis(0, max_index);
-            return charset[ dis(gen) ];
-        };
+        static std::string_view const charset = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+        const size_t max_index = (charset.size() - 1);
+        static std::random_device rd;
+        static std::mt19937_64 gen(rd());
+        std::uniform_int_distribution<> dis(0, max_index);
         std::string str(length,0);
-        std::generate_n(str.begin(), length, randchar);
+        for (size_t i = 0; i < length; i++) {
+            str[i] = charset[dis(gen)];
+        }
         return str;
     }
 }
