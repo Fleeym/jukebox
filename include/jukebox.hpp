d7647e19b5849632275f0ae69a8b78a621394c08
Flafy <flafyarazi@gmail.com>
2024-05-31 02:44:30 +0300

    feat(api): Add getActiveNong, deleteNong, and getDefaultNong (#37)
    Adds customSongWidget to setActiveNong: an optional custom song widget to update
    Renames Song to Nong

--- include/jukebox.hpp
+++ include/jukebox.hpp
@@ -27,6 +27,29 @@ namespace jukebox {
      * 
      * @param song the song to set as active
      * @param songID the id of the song
+     * @param customSongWidget optional custom song widget to update
     */
-    JUKEBOX_DLL void setActiveSong(SongInfo const& song, int songID);
+    JUKEBOX_DLL void setActiveNong(SongInfo const& song, int songID, const std::optional<geode::Ref<CustomSongWidget>>& customSongWidget);
+
+    /**
+     * Gets the active song of a songID
+     * 
+     * @param songID the id of the song
+    */
+    JUKEBOX_DLL std::optional<SongInfo> getActiveNong(int songID);
+
+    /**
+     * Deletes a NONG from the JSON of a songID
+     * 
+     * @param song the song to delete
+     * @param songID the id of the replaced song
+    */
+    JUKEBOX_DLL void deleteNong(SongInfo const& song, int songID);
+
+    /**
+     * Sets the song of the songID as the default song provided by GD
+     * 
+     * @param songID the id of the song
+    */
+    JUKEBOX_DLL std::optional<SongInfo> getDefaultNong(int songID);
 }
