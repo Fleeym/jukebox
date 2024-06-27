ee4dec36089f67a735ec9770c2c6cb541f68eb78
Fleeym <61891787+Fleeym@users.noreply.github.com>
2024-06-27 22:29:27 +0300

    simplify

--- include/nong.hpp
+++ include/nong.hpp
@@ -13,19 +13,19 @@
 namespace jukebox {
 
 struct JUKEBOX_DLL SongMetadata {
-    int m_id;
+    int m_gdID;
     std::string m_name;
     std::string m_artist;
     std::optional<std::string> m_level;
     uint32_t m_startOffset;
 
     SongMetadata(
-        int id,
+        int gdID,
         std::string name,
         std::string artist,
         std::optional<std::string> level = std::nullopt,
         uint32_t offset = 0
-    ) : m_id(id),
+    ) : m_gdID(gdID),
         m_name(name),
         m_artist(artist),
         m_level(level),
@@ -46,41 +46,19 @@ public:
     {}
 
     LocalSong(const LocalSong& other) 
-        : m_path(other.m_path) 
-    {
-        m_metadata = std::make_unique<SongMetadata>(SongMetadata {
-            other.m_metadata->m_id,
-            other.m_metadata->m_name,
-            other.m_metadata->m_artist,
-            other.m_metadata->m_level,
-            other.m_metadata->m_startOffset,
-        });
-    }
+        : m_path(other.m_path),
+        m_metadata(std::make_unique<SongMetadata>(*other.m_metadata))
+    {}
 
     LocalSong& operator=(const LocalSong& other) {
         m_path = other.m_path;
-        m_metadata = std::make_unique<SongMetadata>(SongMetadata {
-            other.m_metadata->m_id,
-            other.m_metadata->m_name,
-            other.m_metadata->m_artist,
-            other.m_metadata->m_level,
-            other.m_metadata->m_startOffset,
-        });
+        m_metadata = std::make_unique<SongMetadata>(*other.m_metadata);
 
         return *this;
     }
 
-    LocalSong(LocalSong&& other)
-        : m_path(std::move(other.m_path)),
-        m_metadata(std::move(other.m_metadata))
-    {}
-
-    LocalSong& operator=(LocalSong&& other) {
-        m_path = std::move(other.m_path);
-        m_metadata = std::move(other.m_metadata);
-
-        return *this;
-    }
+    LocalSong(LocalSong&& other) = default;
+    LocalSong& operator=(LocalSong&& other) = default;
 
     SongMetadata* metadata() const {
         return m_metadata.get();
@@ -111,50 +89,69 @@ public:
 
     YTSong(const YTSong& other)
         : m_youtubeID(other.m_youtubeID),
-        m_path(other.m_path)
-    {
-        m_metadata = std::make_unique<SongMetadata>(SongMetadata {
-            other.m_metadata->m_id,
-            other.m_metadata->m_name,
-            other.m_metadata->m_artist,
-            other.m_metadata->m_level,
-            other.m_metadata->m_startOffset,
-        });
-    }
+        m_path(other.m_path),
+        m_metadata(std::make_unique<SongMetadata>(*other.m_metadata))
+    {}
 
     YTSong& operator=(const YTSong& other) {
         m_youtubeID = other.m_youtubeID;
         m_path = other.m_path;
-        m_metadata = std::make_unique<SongMetadata>(SongMetadata {
-            other.m_metadata->m_id,
-            other.m_metadata->m_name,
-            other.m_metadata->m_artist,
-            other.m_metadata->m_level,
-            other.m_metadata->m_startOffset,
-        });
+        m_metadata = std::make_unique<SongMetadata>(*other.m_metadata);
 
         return *this;
     }
 
-    YTSong(YTSong&& other)
-        : m_metadata(std::move(other.m_metadata)),
-        m_path(std::move(other.m_path)),
-        m_youtubeID(std::move(other.m_youtubeID))
-    {}
-
-    YTSong& operator=(YTSong&& other) {
-        m_youtubeID = std::move(other.m_youtubeID);
-        m_path = std::move(other.m_path);
-        m_metadata = std::move(other.m_metadata);
-
-        return *this;
-    }
+    YTSong(YTSong&& other) = default;
+    YTSong& operator=(YTSong&& other) = default;
 
     SongMetadata* metadata() const {
         return m_metadata.get();
     }
     std::string youtubeID() const {
         return m_youtubeID;
+    }
+    std::optional<std::filesystem::path> path() const {
+        return m_path;
+    }
+};
+
+class JUKEBOX_DLL HostedSong {
+private:
+    std::unique_ptr<SongMetadata> m_metadata;
+    std::string m_url;
+    std::optional<std::filesystem::path> m_path;
+public:
+    HostedSong(
+        SongMetadata&& metadata,
+        std::string&& url,
+        std::optional<std::filesystem::path>&& path = std::nullopt
+    ) : m_metadata(std::make_unique<SongMetadata>(metadata)),
+        m_url(std::move(url)),
+        m_path(std::move(path))
+    {}
+
+    HostedSong(const HostedSong& other)
+        : m_metadata(std::make_unique<SongMetadata>(*other.m_metadata)),
+        m_path(other.m_path),
+        m_url(other.m_url)
+    {}
+
+    HostedSong& operator=(const HostedSong& other) {
+        m_metadata = std::make_unique<SongMetadata>(*other.m_metadata);
+        m_path = other.m_path;
+        m_url = other.m_url;
+
+        return *this;
+    }
+
+    HostedSong(HostedSong&& other) = default;
+    HostedSong& operator=(HostedSong&& other) = default;
+
+    SongMetadata* metadata() const {
+        return m_metadata.get();
+    }
+    std::string url() const {
+        return m_url;
     }
     std::optional<std::filesystem::path> path() const {
         return m_path;
