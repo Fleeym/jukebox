55e0e49b577435b81d0a8c8decf536e59bb47538
Fleeym <61891787+Fleeym@users.noreply.github.com>
2024-10-09 23:15:00 +0300

    fix(MDM): fix onSongGetInfoCompleted crashing

--- src/hooks/music_download_manager.cpp
+++ src/hooks/music_download_manager.cpp
@@ -8,6 +8,9 @@
 #include "Geode/binding/GameLevelManager.hpp"
 #include "Geode/binding/MusicDownloadManager.hpp"
 #include "Geode/binding/SongInfoObject.hpp"
+#include "Geode/cocos/cocoa/CCString.h"
+#include "Geode/loader/Log.hpp"
+#include "Geode/utils/cocos.hpp"
 #include "Geode/utils/string.hpp"
 
 using namespace jukebox;
@@ -34,27 +37,29 @@ gd::string JBMusicDownloadManager::pathForSong(int id) {
 void JBMusicDownloadManager::onGetSongInfoCompleted(gd::string p1,
                                                     gd::string p2) {
     MusicDownloadManager::onGetSongInfoCompleted(p1, p2);
-    auto songID = std::stoi(p2);
+    int songID = std::stoi(p2);
 
-    // CCArray* keys = GameLevelManager::sharedState()->responseToDict(p1,
-    // true)->allKeys();
-    // // Example of p1: 1~|~945695~|~2~|~Tennobyte - Fly
-    // Away~|~3~|~51164~|~4~|~Tennobyte~|~5~|~6.47~|~6~|~~|~10~|~https%3A%2F%2Faudio.ngfiles.com%2F945000%2F945695_Tennobyte---Fly-Away.mp3%3Ff1593523523~|~7~|~~|~11~|~0~|~12~|~~|~13~|~~|~14~|~0
-    // // Values are the keys in the Dictionary. For some reason..
-    // // It also seems like only sometimes empty values are ommited.
+    constexpr size_t SONG_NAME_INDEX = 3;
+    constexpr size_t ARTIST_NAME_INDEX = 7;
+
     CCDictionary* dict =
         GameLevelManager::sharedState()->responseToDict(p1, true);
 
-    // for (int i = 0; i < keys->count(); i++)  {
-    //     log::info("{}", keys->objectAtIndex(i));
-    //     log::info("{}",
-    //     static_cast<CCString*>(keys->objectAtIndex(i))->getCString());
-    // }
-    // GetSongInfoEvent(this->getSongInfoObject(songID), songID).post();
-    GetSongInfoEvent(
-        static_cast<CCString*>(dict->allKeys()->objectAtIndex(3))->getCString(),
-        static_cast<CCString*>(dict->allKeys()->objectAtIndex(7))->getCString(),
-        songID)
+    CCArrayExt<CCString*> keys = CCArrayExt<CCString*>(dict->allKeys());
+
+    // Size 0 -> got an invalid response from the servers
+    if (keys.size() == 0 || keys.size() < ARTIST_NAME_INDEX) {
+        return;
+    }
+
+    CCString* songName = keys[SONG_NAME_INDEX];
+    CCString* artistName = keys[ARTIST_NAME_INDEX];
+
+    if (!songName || !artistName) {
+        return;
+    }
+
+    GetSongInfoEvent(songName->getCString(), artistName->getCString(), songID)
         .post();
 }
 
