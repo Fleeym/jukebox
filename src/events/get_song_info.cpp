6100101c2e4b05abe5177d297fcefc6135a7c659
larzie <lunar@lavache.com>
2024-11-18 16:09:23 +0300

    try a fix

--- src/events/get_song_info.cpp
+++ src/events/get_song_info.cpp
@@ -3,11 +3,19 @@
 
 namespace jukebox::event {
 
-GetSongInfo::GetSongInfo(std::string songName, std::string artistName,
-                         int songId)
+GetSongInfo::GetSongInfo(std::string songName, std::string artistName, int songId)
     : m_songName(songName), m_artistName(artistName), m_gdSongID(songId) {}
-std::string GetSongInfo::songName() { return m_songName; }
-std::string GetSongInfo::artistName() { return m_artistName; }
-int GetSongInfo::gdSongID() { return m_gdSongID; }
+
+std::string GetSongInfo::songName() {
+    return m_songName;
+}
+
+std::string GetSongInfo::artistName() {
+    return m_artistName;
+}
+
+int GetSongInfo::gdSongID() {
+    return m_gdSongID;
+}
 
 }  // namespace jukebox::event
