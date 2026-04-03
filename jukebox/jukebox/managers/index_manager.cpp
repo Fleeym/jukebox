a47d5fee882248c545faab29e764ba694ddb43d4
Noah Coleman <69336552+rednc325@users.noreply.github.com>
2026-04-03 17:47:01 -0500

    Update index_manager.cpp

--- jukebox/jukebox/managers/index_manager.cpp
+++ jukebox/jukebox/managers/index_manager.cpp
@@ -483,7 +483,7 @@ void IndexManager::onDownloadFinish(std::variant<IndexSongMetadata*, Song*>&& so
 }
 
 void IndexManager::registerIndexNongs(Nongs* destination) {
-    if (m_nongsForId.contains(destination->songID())) {
+    if (m_nongsForId.contains(!destination->songID())) {
         return;
     }
 
